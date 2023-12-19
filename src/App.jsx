7d4f384e7bf31e3d187873619eb7563e77129465
Siddharthasaw <sawsiddhartha@gmail.com>
2023-12-19 15:37:30 +0530

    understading props in react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import cardComponent from './Components/cardComponent'
+import './App.css'
+import CardComponent from './Components/cardComponent'
+
+function App() {
+
+
+  return (
+      <>
+          <CardComponent userName = "Siddhartha"  />
+          <CardComponent userName = "Khushi" contact = "contact with my manager"/>
+      </>
+  )
+}
+
+export default App
